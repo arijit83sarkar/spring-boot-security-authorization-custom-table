52fbabb3b660c7cfc0c4de3a5164722c91d520df
arijit83sarkar <mail2arijit83@gmail.com>
2023-01-09 12:33:05 +0530

    code modified

--- src/main/java/com/raven/springbootsecurityauthorizationcustomtable/model/CustomUser.java
+++ src/main/java/com/raven/springbootsecurityauthorizationcustomtable/model/CustomUser.java
@@ -3,6 +3,7 @@ package com.raven.springbootsecurityauthorizationcustomtable.model;
 import org.springframework.data.annotation.Id;
 import javax.persistence.*;
 
+@Entity
 public class CustomUser {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
---
+++ src/main/java/com/raven/springbootsecurityauthorizationcustomtable/model/Employee.java
@@ -0,0 +1,106 @@
+package com.raven.springbootsecurityauthorizationcustomtable.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "full_name", length = 50)
+    private String fullName;
+
+    @Column(name = "phone", length = 15)
+    private String phone;
+
+    @Column(name = "email", length = 60)
+    private String email;
+
+    @Column(name = "user_name", length = 40)
+    private String userName;
+
+    @Column(name = "pwd", length = 200)
+    private String pwd;
+
+    @Column(name = "position", length = 40)
+    private String position;
+
+    public Employee() {
+    }
+
+    public Employee(String fullName, String phone, String email, String userName, String pwd, String position) {
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+        this.userName = userName;
+        this.pwd = pwd;
+        this.position = position;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", userName='" + userName + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
+}
