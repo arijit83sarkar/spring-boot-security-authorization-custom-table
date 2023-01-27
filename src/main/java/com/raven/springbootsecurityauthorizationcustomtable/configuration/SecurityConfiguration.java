6d234a71d689ae3f304d23da957b8d845acb1366
arijit83sarkar <mail2arijit83@gmail.com>
2023-01-28 00:49:00 +0530

    service modification

--- src/main/java/com/raven/springbootsecurityauthorizationcustomtable/configuration/SecurityConfiguration.java
+++ src/main/java/com/raven/springbootsecurityauthorizationcustomtable/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.raven.springbootsecurityauthorizationcustomtable.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -31,8 +32,8 @@ public class SecurityConfiguration {
         return new JdbcUserDetailsManager(dataSource);
     }*/
 
-    /*@Bean
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }*/
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/raven/springbootsecurityauthorizationcustomtable/service/StudentService.java
+++ src/main/java/com/raven/springbootsecurityauthorizationcustomtable/service/StudentService.java
@@ -5,25 +5,38 @@ import com.raven.springbootsecurityauthorizationcustomtable.repository.IStudentR
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentService implements UserDetailsService {
     private final IStudentRepository studentRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public StudentService(IStudentRepository studentRepository) {
+    public StudentService(IStudentRepository studentRepository, PasswordEncoder passwordEncoder) {
         this.studentRepository = studentRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public ResponseEntity<String> registerStudent(Student _student) {
         ResponseEntity<String> responseEntity;
 
         try {
-            Student employeeSaved = this.studentRepository.save(_student);
+            // PASSWORD ENCRYPTION
+            String _pwd = this.passwordEncoder.encode(_student.getPwd());
+            _student.setPwd(_pwd);
+
+            Student studentSaved = this.studentRepository.save(_student);
             responseEntity = ResponseEntity
                     .status(HttpStatus.CREATED)
                     .body("Student details are successfully saved.");
@@ -38,6 +51,19 @@ public class StudentService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        String _username, _password;
+        List<GrantedAuthority> _authorities;
+
+        Student student = this.studentRepository.findByEmail(username);
+        if (student != null) {
+            _username = student.getEmail();
+            _password = student.getPwd();
+            _authorities = new ArrayList<>();
+            _authorities.add(new SimpleGrantedAuthority(student.getRole()));
+        } else {
+            throw new UsernameNotFoundException("Student not found with : " + username);
+        }
+
+        return new User(_username, _password, _authorities);
     }
 }
